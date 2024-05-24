7125010381de6d9331cdb5c281e4993fdb067e7b
tobicodinghub <hornbogent@helmholtzschule.de>
2024-05-24 21:49:01 +0200

    changed requirements to fix CI/CD Pipeline

--- boom/settings.py
+++ boom/settings.py
@@ -183,17 +183,19 @@ USE_I18N = True
 
 USE_TZ = True
 
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-SECURE_SSL_REDIRECT = True
+# SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+# SECURE_SSL_REDIRECT = True
 
-CSRF_COOKIE_SECURE = True
-CSRF_TRUSTED_ORIGINS = ['https://api.boomtechnologies.de']
+# CSRF_COOKIE_SECURE = True
+# CSRF_TRUSTED_ORIGINS = ['https://api.boomtechnologies.de']
 
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = "static/"
+
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
