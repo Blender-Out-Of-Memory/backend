6d74e1480f0d9489a701cf8365f6de1d577b6ae5
tobicodinghub <hornbogent@helmholtzschule.de>
2024-07-07 13:40:13 +0200

    changed apps.py to reflect changes in models

--- TaskScheduler/apps.py
+++ TaskScheduler/apps.py
@@ -2,10 +2,8 @@ from django.apps import AppConfig
 
 
 class TaskschedulerConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'TaskScheduler'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "TaskScheduler"
 
     def ready(self):
-        # imports for including models in migrations
-        # must be set here because apps must be initialized first
-        from .TaskScheduler import RenderTask
+        import TaskScheduler.models  # This ensures models are loaded
