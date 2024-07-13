b79d0c7be3a566e7b18b6c99166709d851bcfe4f
tobicodinghub <hornbogent@helmholtzschule.de>
2024-07-13 19:25:29 +0200

    added some auth

--- TaskScheduler/api.py
+++ TaskScheduler/api.py
@@ -1,14 +1,34 @@
 from rest_framework import viewsets, status
 from rest_framework.decorators import action
 from rest_framework.response import Response
+from rest_framework import permissions
+from rest_framework.permissions import IsAuthenticated
 from django.http import HttpRequest, HttpResponse
 from .models import RenderTask
 from .serializers import RenderTaskSerializer
 from .TaskScheduler import TaskScheduler
 
+class IsOwner(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        return obj.created_by == request.user
+
 class RenderTaskViewSet(viewsets.ModelViewSet):
     queryset = RenderTask.objects.all()
     serializer_class = RenderTaskSerializer
+    permission_classes = [IsAuthenticated]
+
+    def perform_create(self, serializer):
+        serializer.save(created_by=self.request.user)
+
+    def get_queryset(self):
+        return RenderTask.objects.filter(created_by=self.request.user)
+
+    def get_permissions(self):
+        if self.action in ["retrieve", "update", "partial_update", "destroy"]:
+            self.permission_classes = [IsAuthenticated, IsOwner]
+        else:
+            self.permission_classes = [IsAuthenticated]
+        return super(RenderTaskViewSet, self).get_permissions()
 
     @action(detail=False, methods=['post'])
     def run_task(self, request: HttpRequest):
